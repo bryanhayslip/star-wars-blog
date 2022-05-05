401ab5c537b859670bd004772c201efc46e32bd0
bryanhayslip <bryan.hayslip@gmail.com>
2022-05-05 03:38:57 +0000

    added FavoritesContext to planets card

--- src/js/component/planetscard.js
+++ src/js/component/planetscard.js
@@ -9,9 +9,18 @@ export const PlanetsCard = (props) => {
     <div className="border border-success col-3 m-3">
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
+        <div className="card-footer">
         <a href={"/planets/" + props.uid} className="btn btn-primary">
           {props.name}
         </a>
+        { isFavorited ? <button onClick={() => {
+              setFavorites(favorites.filter(uid => uid !== props.uid 
+              ))
+          }}  className="btn btn-success">unfavorite</button> :
+          <button onClick={() => {
+              setFavorites([...favorites, props.uid])
+          }} className="btn btn-warning">favorite</button>}
+        </div>
       </div>
     </div>
   );
